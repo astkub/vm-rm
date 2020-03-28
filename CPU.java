8c0363a26226a55b80ca6d2a0b2267f861da6c53
Kublickaitė Asta <asta.kublickaite@mif.stud.vu.lt>
2020-03-28 14:16:48 +0200

    Added CPU commands

--- CPU.java
+++ CPU.java
@@ -1,5 +1,29 @@
 
 
 public class CPU{
+    private String START = "$STR";
+    private String END = "$END";
+
+    private String ADD0 = "ADD0";
+    private String SUB0 = "SUB0";
+    private String MUL0 = "MUL0";
+    private String DIV0 = "DIV0";
+
+    private String GAx1x2 = "GA";
+    private String GBx1x2 = "GB";
+    private String SAx1x2 = "SA";
+    private String SBx1x2 = "SB";
+    private String PRx1x2 = "PR";
+    private String GDx1x2 = "GD";
+
+    private String DBAx = "DBA";
+    private String UBAx = "UBA";
+    private String LOCx = "LOC";
+    private String UNLx = "UNL";
+
+    private String CMP = "CMP";
+
+    private String HALT = "HALT";
+    private String JMx1x2 = "JM";
 
 }

e7d1ffed941053303cc1692079dd0e343a3ca9cd
Kublickaitė Asta <asta.kublickaite@mif.stud.vu.lt>
2020-03-28 13:48:37 +0200

    Memory interface added

---
+++ Memory.java
@@ -0,0 +1,5 @@
+public interface Memory{
+    public Word read(int address);// throws MemoryException;
+    public void write(Word word, int address);// throws MemoryException;
+    public int getSize();
+}

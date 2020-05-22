133b395daccceff730eb2329e4528bc4a54f6e83
Kublickaitė Asta <asta.kublickaite@mif.stud.vu.lt>
2020-05-22 15:30:23 +0300

    Resourses class added

---
+++ Resources.java
@@ -0,0 +1,64 @@
+
+
+public class Resources{
+  class MOS_pabaiga{
+    
+  }
+  class isVartotojoSasajos{
+
+  }
+  class isGetLine{
+
+  }
+  class duomenu_vieta{
+
+  }
+  class isLoader{
+
+  }
+  class uzduotis_supervizorineje_atmintyje{
+
+  }
+  class irasyta_i_atminti{
+
+  }
+  class vartotojo_atmintis{
+
+  }
+  class isInterrupt{
+
+  }
+  class laukti_ivedimo{
+
+  }
+  class eilute_atmintyje{
+
+  }
+  class isPrintLine{
+
+  }
+  class semaforas{
+
+  }
+  class isMemoryGovernor{
+
+  }
+  /*class neegzistuojantis_resursas{
+
+  }*/
+  class continue{
+
+  }
+  class interrupt{
+
+  }
+  class kanalas_1{
+
+  }
+  class kanalas_2{
+
+  }
+  class kanalas_3{
+
+  }
+}

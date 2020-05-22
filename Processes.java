098def25ad1c3f97b500e9ffb1f44d69d605f0d9
Kublickaitė Asta <asta.kublickaite@mif.stud.vu.lt>
2020-05-22 17:51:08 +0300

    Process interface and enums

--- Processes.java
+++ Processes.java
@@ -1,101 +1,130 @@
 import java.util.ArrayList;
 
-enum ProcessID{
-  START_STOP,
-  READ_FROM_INTERFACE,
-  MAIN_PROC,
+enum ProcessPriority{
   JOB_GOVERNOR,
   VIRTUAL_MACHINE,
-  INTERRUPT,
   PRINT_LINE,
   GET_LINE,
-  LOADER,
   WRITE_TO_MEMORY,
-  MEMORY_GOVERNOR
+  LOADER,
+  READ_FROM_INTERFACE,
+  MAIN_PROC,
+  MEMORY_GOVERNOR,
+  INTERRUPT,
+  START_STOP
+}
+
+enum ProcessState{
+   RUNNING, 
+   BLOCKED,
+   BLOCKED_STOPPED,
+   READY_STOPPED,
+   READY
+}
+
+interface Process{
+   public void work();
 }
 
 public class Processes{
-  private List runningProcesses = new ArrayList();
-  private int descriptorSpace = 4; //nezinau kiek kolkas
-  private int[] descriptor = new int[descriptorSpace]; //dinaminis objektas //proceso aprasas //laikomi visi reikalingi parametrai kaip virtualios masinos registru reksmes ir kiti kintamieji
+   private ArrayList<Process> runningProcesses = new ArrayList<Process>();
+   private int descriptorSpace = 4; //nezinau kiek kolkas
+   private int[] descriptor = new int[descriptorSpace]; //dinaminis objektas //proceso aprasas //laikomi visi reikalingi parametrai kaip virtualios masinos registru reksmes ir kiti kintamieji
 
-  public void createProcess(Processes parent, String state, int priority, List<String, Integer> elements, String name){
+   public void createProcess(Process parent, ProcessState state, ProcessPriority priority, ArrayList<String> elements, String name){
       //kuriamas procesas
       //procesas registruojamas bendrajame procesu sarase, tevo sunu sarase
       //skaiciuojamas vidinis identifikacijos numeris
       //sukuriamas vaiku procesu sarasas(tuscias), sukurtu resursu sarasas ir t.t
       planner();
-  }
+   }
 
-  public void killProcess(){
+   public void killProcess(){
       //Pradedama naikinti proceso sukurtus resursus ir vaikus
       //išmetamas iš tevo sukurtu procesu saraso
       //ismetamas is bendro procesu saraso ir, jei reikia, is pasiruosusiu resursu saraso
       //naikinami visi jam perduoti resursai ir proceso descriptorius yra sunaikinamas
       planner();
-  }
+   }
 
-  public void stopProcess(){
+   public void stopProcess(){
       //Keiciama proceso busena is blokuotos i blokuota sustabdyta ar is pasiruosusios i pasiruosiusia sustabdyta.
       //Einamasis procesas stabdomas tampa pasiruosusiu sustabdytu 
       //planner();
-  }
+   }
 
-  public void activateProcess(){
+   public void activateProcess(){
       //Keiciama proceso busena is blokuotos sustabdytos i blokuota ar is pasiruosiusi sustabdyta i pasiruosusi.
       //planner();
-  }
+   }
 
-  public void planner(){
+   public void planner(){
       //skirsto resursus, procesoriu.
-  }
+   }
 
-  interface Process{
-    public void work(Resources resource);
-  }
+   class StartStop implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}
+   }
 
-  class StartStop implements Process{
-      public void work(Resources resource){}
-  }
+   class ReadFromInterface implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}
+   }
 
-  class ReadFromInterface implements Process{
-      public void work(Resources resource){}
-  }
+   class MainProc implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}
+   }
 
-  class MainProc implements Process{
-      public void work(Resources resource){}
-  }
+   class JobGovernor implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}
+   }
 
-  class JobGovernor implements Process{
-      public void work(Resources resource){}
-  }
+   class VirtualMachine implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}
+   }
 
-  class VirtualMachine implements Process{
-      public void work(Resources resource){}
-  }
+   class Interupt implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}
+   }
 
-  class Interupt implements Process{
-      public void work(Resources resource){}
-  }
+   class PrintLine implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}
+   }
 
-  class PrintLine implements Process{
-      public void work(Resources resource){}
-  }
+   class GetLine implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}
+   }
 
-  class GetLine implements Process{
-      public void work(Resources resource){}
-  }
+   class Loader implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}
+   }
 
-  class Loader implements Process{
-      public void work(Resources resource){}
-  }
+   class WriteToMemory implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}
+   }
 
-  class WriteToMemory implements Process{
-      public void work(Resources resource){}
-  }
-
-  class MemoryGovernor implements Process{
-      public void work(Resources resource){}  
-  }
-
+   class MemoryGovernor implements Process{
+      public ProcessPriority priority;
+      public ProcessState state;
+      public void work(){}  
+   }
 }
--- Resources.java
+++ Resources.java
@@ -7,60 +7,78 @@ public class Resources{
     void addToQueue(){}
   }
   class isVartotojoSasajos{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class isGetLine{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class duomenu_vieta{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class isLoader{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class uzduotis_supervizorineje_atmintyje{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class irasyta_i_atminti{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class vartotojo_atmintis{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class isInterrupt{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class laukti_ivedimo{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class eilute_atmintyje{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class isPrintLine{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class semaforas{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class isMemoryGovernor{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   /*class neegzistuojantis_resursas{
 
   }*/
-  class continue{
-
+  class continuee{
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class interrupt{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class kanalas_1{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class kanalas_2{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
   class kanalas_3{
-
+    Queue<Integer> queue = new LinkedList<Integer>();
+    void addToQueue(){}
   }
 }
